22841e23c93d0d44d73b1712ddd221f6b3b5dc3f
Manuel <caballeromanuel203@gmail.com>
2025-04-26 21:13:52 +0800

    added new migrations for modified persons column types

--- Migrations/20250426130802_ModifiedPersonMigration.Designer.cs
+++ Migrations/20250426130802_ModifiedPersonMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PersonManagementApp.Data;
 
@@ -11,9 +12,11 @@ using PersonManagementApp.Data;
 namespace PersonManagementApp.Migrations
 {
     [DbContext(typeof(MyAppContext))]
-    partial class MyAppContextModelSnapshot : ModelSnapshot
+    [Migration("20250426130802_ModifiedPersonMigration")]
+    partial class ModifiedPersonMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PersonManagementApp.Models;
 
 public class Person
 {
     public int PersonId { get; set; }
-    required public string FirstName { get; set; }
-    required public string LastName { get; set; }
-    required public string BirthPlace { get; set; }
-    required public string Gender { get; set; }
-    required public string Religion { get; set; }
     required public DateOnly DateOfBirth { get; set; }
+
+
+    [Column(TypeName = "varchar(50)")]
+    required public string FirstName { get; set; }
+
+    [Column(TypeName = "varchar(50)")]
+    required public string LastName { get; set; }
+
+    [Column(TypeName = "varchar(100)")]
+    required public string BirthPlace { get; set; }
+
+    [Column(TypeName = "varchar(20)")]
+    required public string Gender { get; set; }
+
+    [Column(TypeName = "varchar(100)")]
+    required public string Religion { get; set; }
 }
